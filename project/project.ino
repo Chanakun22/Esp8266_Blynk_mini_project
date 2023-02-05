10847f2be443223faf08a42d87a8f3667b92499c
Chanakun22 <chanakun428@gmail.com>
2023-02-05 11:14:21 +0700

    Version 4 GOOD Version
    #fix all bug
    #no error
    
    #is good

--- project/project.ino
+++ project/project.ino
@@ -16,13 +16,11 @@ int LED_PIN = D0;
 //#define USE_NODE_MCU_BOARD
 //#define USE_WITTY_CLOUD_BOARD
 #define USE_WEMOS_D1_MINI
+
 #include <Wire.h>
 #include <LiquidCrystal_I2C.h>
 LiquidCrystal_I2C lcd(0x27, 20, 4);
 #include "BlynkEdgent.h"
-#include <FirebaseESP8266.h>
-#define FIREBASE_HOST "projecy-31eb8-default-rtdb.asia-southeast1.firebasedatabase.app"
-#define FIREBASE_KEY "8rC32YDSJr5hJCDjIRk9CHvPpmXOAV5DvGXpHpmP"
 #include <PZEM004Tv30.h>
 #include <SoftwareSerial.h>
 #if defined(ESP32)
@@ -35,15 +33,9 @@ LiquidCrystal_I2C lcd(0x27, 20, 4);
  e
 */ 
 
-#if !defined(PZEM_RX_PIN) && !defined(PZEM_TX_PIN)
-#define PZEM_RX_PIN D5
-#define PZEM_TX_PIN D6
-#endif
-SoftwareSerial pzemSWSerial(PZEM_RX_PIN, PZEM_TX_PIN);
-PZEM004Tv30 pzem(pzemSWSerial);
+
  
 bool t = true;
-FirebaseData firebaseData;
 bool state = false;
 WidgetLED led1(V7);
 #define BLYNK_GREEN     "#23C48E"
@@ -55,28 +47,20 @@ WidgetLED led1(V7);
 
 BlynkTimer timer; 
 
+PZEM004Tv30 pzem(D5, D6);
 
 
 
 BLYNK_CONNECTED() {
-  if(Firebase.getInt(firebaseData, "/state/Status_blynk_ledV3")) {
-    int val = firebaseData.intData();
-    Blynk.virtualWrite(V4, val);
-    Blynk.virtualWrite(V3, val);
-    digitalWrite(LED_PIN,!val);
-    digitalWrite(D4,!val);
-  } 
-  else {
-    Serial.println("Error : " + firebaseData.errorReason());
-  }
+  lcd.begin();
+  lcd.backlight();
 }
 
 
 
 BLYNK_WRITE(V3){
   int value = param.asInt();
-  // digitalWrite(D0,!value);
-  Firebase.setInt(firebaseData, "/state/Status_blynk_ledV3", value);
+  // digitalWrite(D0,!value)
   digitalWrite(LED_PIN,!value);
   digitalWrite(D4,!value);
 }
@@ -84,41 +68,27 @@ BLYNK_WRITE(V3){
 
 
 
+
+
 void setup()
 {
     
-  lcd.begin();
-  lcd.backlight();
+  timer.setInterval(2000L,readsensor);
   Serial.begin(115200);
   delay(100);
   pinMode(LED_PIN,OUTPUT);
   pinMode(D4,OUTPUT);
   BlynkEdgent.begin();
-  Firebase.begin(FIREBASE_HOST, FIREBASE_KEY);
   
 
 
 }
 
 
-void loop() {
-  BlynkEdgent.run();
-    // state = !state;
-    // if(Firebase.setInt(firebaseData, "/state", state)) {
-    //     Serial.println("Added"); 
-    // } else {
-    //     Serial.println("Error : " + firebaseData.errorReason());
-    // }
-
-  if(Firebase.getInt(firebaseData, "/state/Status_blynk_ledV3")) {
-    int val = firebaseData.intData();
-    Blynk.virtualWrite(V4, val);
-  } 
-  else {
-    Serial.println("Error : " + firebaseData.errorReason());
-  }
 
 
+void readsensor(){
+  
 
         //Serial.print("Custom Address:");
     // Serial.println(pzem.readAddress(), HEX);
@@ -149,11 +119,50 @@ void loop() {
         }
         
     } else if (isnan(current)) {
-        Serial.println("Error reading current");
+          Blynk.logEvent("alert", "Error reading current");
+        // Blynk.virtualWrite(V7, 0);
+        led1.setColor(BLYNK_RED);
+        if(t){
+          t=false;
+          lcd.clear();
+          lcd.setCursor(5,1);
+          lcd.print("Sensor Eror");
+          Blynk.virtualWrite(V0, 0);
+          Blynk.virtualWrite(V1, 0);
+          Blynk.virtualWrite(V2, 0);
+          Blynk.virtualWrite(V5, 0);
+          Blynk.virtualWrite(V6, 0);
+        }
     } else if (isnan(power)) {
-        Serial.println("Error reading power");
+          Blynk.logEvent("alert", "Error reading current");
+        // Blynk.virtualWrite(V7, 0);
+        led1.setColor(BLYNK_RED);
+        if(t){
+          t=false;
+          lcd.clear();
+          lcd.setCursor(5,1);
+          lcd.print("Sensor Eror");
+          Blynk.virtualWrite(V0, 0);
+          Blynk.virtualWrite(V1, 0);
+          Blynk.virtualWrite(V2, 0);
+          Blynk.virtualWrite(V5, 0);
+          Blynk.virtualWrite(V6, 0);
+        }
     } else if (isnan(energy)) {
-        Serial.println("Error reading energy");
+          Blynk.logEvent("alert", "Error reading current");
+        // Blynk.virtualWrite(V7, 0);
+        led1.setColor(BLYNK_RED);
+        if(t){
+          t=false;
+          lcd.clear();
+          lcd.setCursor(5,1);
+          lcd.print("Sensor Eror");
+          Blynk.virtualWrite(V0, 0);
+          Blynk.virtualWrite(V1, 0);
+          Blynk.virtualWrite(V2, 0);
+          Blynk.virtualWrite(V5, 0);
+          Blynk.virtualWrite(V6, 0);
+        }
     } else if (isnan(frequency)) {
         Serial.println("Error reading frequency");
     } else if (isnan(pf)) {
@@ -171,11 +180,11 @@ void loop() {
       lcd.setCursor(0,0);
       lcd.print("Vlotage: "+String(voltage)+" V ");
       lcd.setCursor(0,1);
-      lcd.print("Current: "+String(current)+" A     ");
+      lcd.print("Current: "+String(current)+" A  ");
       lcd.setCursor(0,2);
-      lcd.print("Power:   "+String(power)+" W    ");
+      lcd.print("Power:   "+String(power)+" W  ");
       lcd.setCursor(0,3);
-      lcd.print("energy:  "+String(energy)+" Kwh   ");
+      lcd.print("energy:  "+String(energy)+" Kwh  ");
       // Serial.print("Voltage: ");      Serial.print(voltage);      Serial.println("V");
       //  Serial.print("Current: ");      Serial.print(current);      Serial.println("A");
       //  Serial.print("Power: ");        Serial.print(power);        Serial.println("W");
@@ -183,8 +192,17 @@ void loop() {
       //  Serial.print("Frequency: ");    Serial.print(frequency, 1); Serial.println("Hz");
       //  Serial.print("PF: ");           Serial.println(pf);
     }
-delay(10);
 }
+
+
+
+void loop() {
+  BlynkEdgent.run();
+  timer.run();
+}
+
+
+
 
 
 
